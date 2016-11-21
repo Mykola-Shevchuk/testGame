4d1ed857155d8ef41db28e294973f9d77440773f
Mykola-Shevchuk <mykola.shevchuk.ua@gmail.com>
2016-11-21 15:03:13 +0200

    pre rel

--- js/main.js
+++ js/main.js
@@ -9,8 +9,14 @@ $(document).ready(function($) {
 		$gameInfoIcon = $('.more-information'),
 		$gameInfoBlock = $('.game-information-section'),
 		$closerFullInfo = $('.close-full-information'),
+		$closeGame = $('.close > span');
 		$hiderFullInfo = $('.hide'),
-		$bodyWidth = $('body').width();
+		$bodyWidth = $('body').width(),
+		$favoriteStar = $('.favorite-star'),
+		$fullScreen = $('.fullscreen'),
+		$deposite = $('.deposit'),
+		$popupBlock = $('.popup'),
+		$closeModalWindow = $('.close-modal-window');
 
 	//Add active class
 	$($itemMenu).on('click', function() {
@@ -29,8 +35,25 @@ $(document).ready(function($) {
 	});
 
 
-//CLOSERS
+	//CLOSERS GAME 
+
+	$gameWindow.on('click', function(event) {
+		if (event.target === $closerFullInfo[0]) {
+			$gameWindow.fadeOut(400);
+		}
+
+		if (event.target === $closeGame[0]) {
+			$gameWindow.fadeOut(400);
+		}
+
+	});
+
+
+	//ToggleSlide Game Menu
+
 	$gameInfoIcon.on('click', function() {
+
+		// Adaptive for screen less 980px
 
 		if ($bodyWidth < 980) {
 			$gameNavigationBlock.fadeOut(500);
@@ -44,6 +67,8 @@ $(document).ready(function($) {
 			return false;
 		};
 
+		// Adaptive for screen more 980px
+
 		$gameNavigationBlock.fadeOut(500);
 
 		$gameWindow.css({
@@ -51,21 +76,14 @@ $(document).ready(function($) {
 		});
 
 		
-			$gameInfoBlock.delay(500).fadeIn(500);
-		
-
+		$gameInfoBlock.delay(500).fadeIn(500);
 	});
 
-	$closerFullInfo.on('click', function() {
-		$gameWindow.css({
-			width: '900px',
-		}); 
-
-		$gameNavigationBlock.fadeIn(500);
-
-	});
+	// Hider Game Menu
 
 	$hiderFullInfo.on('click', function() {
+
+		// Adaptive for screen less 980px
 
 		if ($bodyWidth < 980) {
 			$gameInfoBlock.fadeOut(500);
@@ -79,6 +97,8 @@ $(document).ready(function($) {
 			return false;
 		};
 
+		// Adaptive for screen more 980px
+
 		$gameInfoBlock.css('display', 'none');
 		$gameWindow.css({
 			width: $gameWindow.innerWidth() - $gameInfoBlock.innerWidth()
@@ -86,6 +106,100 @@ $(document).ready(function($) {
 
 		$gameNavigationBlock.fadeIn('fast');
 
+	});
+
+	// ADD STAR
+
+	$favoriteStar.on('click', function() {
+		$(this).toggleClass('show-favorite-star');
+		
+	});
+
+	// POPUP
+
+	$deposite.on('click', function() {
+		$popupBlock.fadeIn(400);
+	});
+
+	$closeModalWindow.on('click', function() {
+		$popupBlock.fadeOut(400);
+	});
+
+	//FULL SCREEN
+
+	$fullScreen.on('click', function() {
+		function toggleFullScreen(elem) {
+	    
+		    if ((document.fullScreenElement !== undefined &&
+		    	 document.fullScreenElement === null) ||
+		    	 (document.msFullscreenElement !== undefined &&
+		    	  document.msFullscreenElement === null) ||
+		    	   (document.mozFullScreen !== undefined &&
+		    	    !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined &&
+		    	     !document.webkitIsFullScreen)) {
+
+		        if (elem.requestFullScreen) {
+		            elem.requestFullScreen();
+		        } else if (elem.mozRequestFullScreen) {
+		            elem.mozRequestFullScreen();
+		        } else if (elem.webkitRequestFullScreen) {
+		            elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+		        } else if (elem.msRequestFullscreen) {
+		            elem.msRequestFullscreen();
+		        }
+		    } else {
+		        if (document.cancelFullScreen) {
+		            document.cancelFullScreen();
+		        } else if (document.mozCancelFullScreen) {
+		            document.mozCancelFullScreen();
+		        } else if (document.webkitCancelFullScreen) {
+		            document.webkitCancelFullScreen();
+		        } else if (document.msExitFullscreen) {
+		            document.msExitFullscreen();
+		        }
+		    }
+		}
+
+		toggleFullScreen(document.body)
 
 	});
+
+
+	/*FORM FILTER*/
+
+	$(".popup-form-content input").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+             // Allow: Ctrl+A, Command+A
+            (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || 
+             // Allow: home, end, left, right, down, up
+            (e.keyCode >= 35 && e.keyCode <= 40)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+
+	/*Отслеживаем изминения размеров экрана*/
+
+	/*var resize = function(e){
+		console.log($gameWindow.innerWidth());
+		console.log($gameWindow.width());
+  		$gameWindow.attr('style', '');
+	};
+
+	(function(){
+		var time;
+		window.onresize = function(e){
+			if (time)
+				clearTimeout(time);
+				time = setTimeout(function(){
+				resize(e);
+			}, 100);
+		}
+	})();*/
+
 });
